6ef76973b5e95507a953c7c2910fe0d06cc51e24
ShoaibTamboli <shoaibtamboli03@gmail.com>
2021-09-14 12:36:16 +0530

    Understanding DOM

--- js/Tut11.js
+++ js/Tut11.js
@@ -2,3 +2,32 @@
 
 
 
+let a =document;
+ a=document.all;
+// a=document.body;
+// a=document.forms[0];
+
+// Array.from(a).forEach means a ki madadt se banao ek array. aur fir usko iterate karo.
+Array.from(a).forEach(function (element){
+console.log(element)
+});
+
+let b=document.links[0].href;
+console.log(b)
+
+
+let c=document.images;
+
+Array.from(c).forEach(function (element) {
+    console.log(element)
+});
+//console.log(c);
+
+
+
+let d= document.scripts;
+
+Array.from(d).forEach(function (abc) {
+    console.log(abc)
+})
+//console.log(d)
